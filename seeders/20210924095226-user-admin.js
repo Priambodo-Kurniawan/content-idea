c6aa1446977874d24a1d5964e5a6a47bc1834d5f
iam <priambodo@petz.sg>
2021-11-09 07:27:13 +0700

    update seeder

--- seeders/20210924095226-user-admin.js
+++ seeders/20210924095226-user-admin.js
@@ -1,5 +1,7 @@
 "use strict";
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const { hashPassword } = require("../helpers/bcrypt");
 
 module.exports = {
