a21bc5aec2d84e314e5ab6640578a40dbb16de70
iam <priambodo@petz.sg>
2022-02-17 08:32:05 +0700

    google search v2

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -1,4 +1,5 @@
 const googleIt = require("google-it-safesearch");
+const wtj = require("website-to-json");
 
 class ApiController {
   static async search(req, res, next) {
@@ -9,6 +10,52 @@ class ApiController {
         disableConsole: true,
       });
       res.send({ data });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  static async searchV2(req, res, next) {
+    try {
+      let q = req.query.q;
+      if (!q) {
+        throw {
+          name: "BadRequest",
+          msg: "Ops... please input q (query)",
+        };
+      }
+
+      let response = await wtj.extractData(
+        "https://www.google.com/search?q=" + q,
+        {
+          fields: ["data"],
+          parse: function ($) {
+            let content = $("a > h3")
+              .map(function (val) {
+                let link = $(this)
+                  .parent()
+                  .attr("href")
+                  .split("q=")[1]
+                  .split("&sa")[0];
+                let content = $(this).parent().parent().next().text();
+                return {
+                  title: $(this).text(),
+                  link: link,
+                  content,
+                };
+              })
+              .get();
+            return {
+              content,
+            };
+          },
+        }
+      );
+
+      res.status(200).json({
+        response,
+      });
     } catch (error) {
       console.log(error);
       next(error);
--- routes/search.js
+++ routes/search.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const SearchController = require("../controllers/SearchController");
 
 router.get("/", SearchController.search);
+router.get("/v2", SearchController.searchV2);
 
 module.exports = router;
